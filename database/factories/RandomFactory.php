b662b6e60e81d06a1fac4066b419297fc91468bd
Anthony_Uy <anthonyu@posbang.com>
2021-10-22 17:03:14 +0800

    first commit

---
+++ database/factories/RandomFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Random;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RandomFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Random::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_10_21_070250_create_breakdowns_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBreakdownsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('breakdowns', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('value');
+            $table->integer('random_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('breakdowns');
+    }
+}
